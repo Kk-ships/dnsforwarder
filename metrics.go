d164ee031504dd1d212d89dd30422529c1edf8f4
Kaustubh Shirpurkar <kaustubh.shirpurkar@gmail.com>
2025-07-25 09:26:52 +0530

    refactor: integrate metrics recorder and enable pprof for development profiling

--- metrics.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"dnsloadbalancer/metric"
-)
-
-var metricsRecorder = metric.MetricsRecorderInstance
-var StartMetricsUpdater = metric.StartMetricsUpdater
-var StartMetricsServer = metric.StartMetricsServer
